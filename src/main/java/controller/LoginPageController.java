053f2a62e5795930d2a6c520cbbf337de4d920e8
Foyezur Rahman <foyezurrahmanqa@gmail.com>
2020-08-08 11:43:31 -0400

    create new css locator for sign in button.

--- src/main/java/controller/LoginPageController.java
+++ src/main/java/controller/LoginPageController.java
@@ -19,12 +19,12 @@ public class LoginPageController {
     public LoginPageController(WebDriver driver){
         PageFactory.initElements(driver,this);}
 
-//   public void signinButton(WebDriver driver){
-//        // WebElement element =driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a"));
-//        // element.isDisplayed();
-//        //System.out.println(element);
-//        driver.findElement(By.cssSelector("#header > div.nav > div > div > nav > div.header_user_info > a")).click();
-//    }
+   public void signinButton(WebDriver driver){
+        // WebElement element =driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a"));
+        // element.isDisplayed();
+        //System.out.println(element);
+        driver.findElement(By.cssSelector("#header > div.nav > div > div > nav > div.header_user_info > a")).click();
+    }
     public void logIn(){
     signinTab.click();
     inputEmail.sendKeys("test@gmail.com");
